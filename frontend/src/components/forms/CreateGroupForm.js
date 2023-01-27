8799dc29a3cffafa5b58fb8df4cefade77cc7ed4
Cyril Wang <cyrilwang@umail.ucsb.edu>
2023-01-27 12:26:05 -0800

    cw - added margins for better spacing

--- frontend/src/components/forms/CreateGroupForm.js
+++ frontend/src/components/forms/CreateGroupForm.js
@@ -1,5 +1,4 @@
 import { Button, Form } from "react-bootstrap";
-import {Text} from "react-native";
 // import { useForm } from "react-hook-form";
 
 function CreateGroupForm( {submitAction} ) {
@@ -12,7 +11,7 @@ function CreateGroupForm( {submitAction} ) {
     return (
       // <Form onSubmit={handleSubmit(submitAction)}>
       <div >
-        <Form style={{fontSize: "25px"}} className = "text-decoration-underline d-flex justify-content-center align-items-center">
+        <Form style={{fontSize: "25px"}} className = "d-flex justify-content-center align-items-center">
           <fieldset>
             <label>
               <p>Name</p>
@@ -20,8 +19,7 @@ function CreateGroupForm( {submitAction} ) {
             </label>
           </fieldset>
         </Form>
-        <Text>{'\n'}</Text>
-        <Form style={{fontSize: "25px"}} className = "text-decoration-underline d-flex justify-content-center align-items-center">
+        <Form style={{fontSize: "25px", marginBottom: "30px", marginTop: "30px"}} className = "d-flex justify-content-center align-items-center">
           <fieldset>
             <label>
               <p>Group Description</p>
@@ -29,8 +27,6 @@ function CreateGroupForm( {submitAction} ) {
             </label>
           </fieldset>
         </Form>
-        <Text>{'\n\n\n'}</Text>
-
         <Form className = "text-decoration-underline d-flex justify-content-center align-items-center">
           <Button type="submit" data-testid="CreateGroupForm-Create-Button" style={{fontSize: "30px"}}>Create!</Button>
         </Form>
--- frontend/src/pages/CreatePage.js
+++ frontend/src/pages/CreatePage.js
@@ -8,7 +8,7 @@ const CreateGroupPage = () => {
     return (
         <Container expand="xl" className="pt-4 flex-grow-1">
             <div className = "">
-                <h1 className = "text-decoration-underline d-flex justify-content-center align-items-center" style={{ height: "300px", fontSize: "75px" }}>Create a Group</h1>
+                <h1 className = "text-decoration-underline d-flex justify-content-center align-items-center" style={{ height: "250px", fontSize: "75px" }}>Create a Group</h1>
             </div>
             <CreateGroupForm></CreateGroupForm>
         </Container>
