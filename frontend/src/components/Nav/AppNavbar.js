147ee9947e7417621b59b0b97b0267b2fe0b248d
Dylan Chung <yongtak@ucsb.edu>
2023-01-31 16:10:24 -0800

    Revert "Merge pull request #41 from ucsb-cs148-w23/ll-SignInButton2"
    This reverts commit 1152dc408faf26b7d556cb264da9b597a4168cc5, reversing
    changes made to 92e4934856320d2d7a4b8092a831b76986eb593d.

--- frontend/src/components/Nav/AppNavbar.js
+++ frontend/src/components/Nav/AppNavbar.js
@@ -1,20 +1,19 @@
 import { Navbar, Nav, Container, Button } from 'react-bootstrap';
-import SignInButton from '../SignInButton/SignInButton';
 
 function AppNavbar() {
-  return (
-    <>
-      <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="/">Calendar App</Navbar.Brand>
-          <Nav className="ml-auto">
-            <Nav.Link href="/home">Home</Nav.Link>
-            <SignInButton></SignInButton>
-          </Nav>
-        </Container>
-      </Navbar>
-    </>
-  );
+    return (
+        <>
+            <Navbar bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="/">Calendar App</Navbar.Brand>
+                    <Nav className="ml-auto">
+                        <Nav.Link href="/home">Home</Nav.Link>
+                        <Button href="/login">Login</Button>
+                    </Nav>
+                </Container>
+            </Navbar>
+        </>
+    );
 }
 
 export default AppNavbar;
