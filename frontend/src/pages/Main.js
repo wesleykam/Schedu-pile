1348311a49e06e42abf1ed9a5701dc683069b441
DylanC1222 <77405374+DylanC1222@users.noreply.github.com>
2023-02-06 21:57:06 -0800

    dc - try it now button goes to auth" (#91)

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,25 +1,17 @@
 import React from 'react';
 import '../style/Home.css';
-import { Link, useNavigate } from 'react-router-dom';
 
 function Main() {
-  const navigate = useNavigate();
   return (
     <div className="Home">
       <div></div>
       <div className="headerContainer">
         <h1>When2Meet2.0</h1>
         <p>Find group avaliable time by importing from google calender</p>
-        <button
-          onClick={() => {
-            navigate('/home');
-          }}
-        >
-          <Link to="/create" style={{ textDecoration: 'none', color: '#FFF' }}>
-            {' '}
-            TRY IT NOW{' '}
-          </Link>
-        </button>
+
+        <a href="http://localhost:8000/auth/google">
+          <button>TRY IT NOW </button>
+        </a>
       </div>
     </div>
   );
