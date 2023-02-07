7e5a9b33d92adc33afdb6d00b0d30ab5266721b6
DylanC1222 <77405374+DylanC1222@users.noreply.github.com>
2023-02-06 21:58:10 -0800

    dc - made navbar change on auth status (#90)

--- frontend/src/components/Nav/AppNavbar.js
+++ frontend/src/components/Nav/AppNavbar.js
@@ -7,17 +7,18 @@ function AppNavbar({ user }) {
       <Navbar bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="/">Calendar App</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="/create" style={{ marginTop: '3px' }}>
-              Create Group
-            </Nav.Link>
-            <Nav.Link href="/groups" style={{ marginTop: '3px' }}>
-              Groups
-            </Nav.Link>
-          </Nav>
-
+          {user.authenticated ? (
+            <Nav className="me-auto">
+              <Nav.Link href="/create" style={{ marginTop: '3px' }}>
+                Create Group
+              </Nav.Link>
+              <Nav.Link href="/groups" style={{ marginTop: '3px' }}>
+                Groups
+              </Nav.Link>
+            </Nav>
+          ) : null}
           <Nav className="ml-auto">
-            <Nav.Link href="/home" style={{ marginRight: '10px' }}>
+            <Nav.Link href="/" style={{ marginRight: '10px' }}>
               Home
             </Nav.Link>
             <AuthButton user={user} />
