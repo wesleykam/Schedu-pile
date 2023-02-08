623537dc5e9ab0327a78e7ff7bd341317afbe2ea
lukewli <86935334+lukewli@users.noreply.github.com>
2023-02-07 23:09:19 -0800

    ll - Adding a page for specific groups showing members etc. (#93)
    * Fetching Data
    
    * dc - all pages redirect if not logged in (#92)
    
    * dc - try it now button goes to auth" (#91)
    
    * dc - made navbar change on auth status (#90)
    
    * Fetching Data
    
    * ll - List is populated
    
    * Updated Formatting
    
    * ll - More Formatting
    
    ---------
    
    Co-authored-by: Luke Li <lukewli@ucsb.edu>
    Co-authored-by: DylanC1222 <77405374+DylanC1222@users.noreply.github.com>

--- frontend/src/pages/GroupPage.js
+++ frontend/src/pages/GroupPage.js
@@ -1,11 +1,33 @@
-import React, { useEffect } from 'react';
-import { Container } from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
+import DefaultLayout from '../layouts/DefaultLayout';
+import {
+  Button,
+  Container,
+  CloseButton,
+  Modal,
+  ListGroup,
+  Row,
+  Col,
+} from 'react-bootstrap';
 import AddGroupMembersForm from '../components/forms/AddGroupMembersForm';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const GroupPage = ({ user }) => {
+const CLASSNAME = 'd-flex justify-content-center align-items-center';
+let nextId = 0;
+
+export default function GroupDetails({ user }) {
   console.log(user);
   const navigate = useNavigate();
+  const [members, setMembers] = useState([]);
+  const [edit, setEdit] = useState(false);
+  const [show, setShow] = useState(false);
+  const [del_email, setDelete] = useState('');
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  const path = window.location.pathname;
+  let url =
+    'http://localhost:8000/api/group' + path.substring(path.lastIndexOf('/'));
 
   useEffect(() => {
     fetch('http://localhost:8000/check', {
@@ -20,13 +42,109 @@ const GroupPage = ({ user }) => {
       if (response.status === 200) return response.json();
       navigate('/');
     });
-  });
+
+    fetch(url, {
+      method: 'GET',
+    })
+      .then((response) => {
+        if (response.status === 200) {
+          return response.json();
+        }
+        throw new Error('failed to fetch group details');
+      })
+
+      .then((responseJson) => {
+        console.log(responseJson.groupMembers);
+        setMembers(responseJson.groupMembers);
+      });
+  }, [edit]);
 
   return (
-    <Container expand="xl" className="pt-4 flex-grow-1">
-      <AddGroupMembersForm></AddGroupMembersForm>
-    </Container>
+    <DefaultLayout className={CLASSNAME}>
+      <Container fluid>
+        <Row className="mb-3">
+          <Col></Col>
+          <Col
+            xs={6}
+            className="d-flex justify-content-center align-items-center mx-auto fs-1"
+          >
+            Group Details
+          </Col>
+          <Col>
+            <Button
+              className="d-flex justify-content-center align-items-center mx-auto"
+              onClick={() => {
+                setEdit((prevEdit) => !prevEdit);
+              }}
+            >
+              Edit
+            </Button>
+          </Col>
+        </Row>
+        <Row className="mb-3">
+          <Col></Col>
+          <Col
+            xs={6}
+            className="d-flex justify-content-center align-items-center mx-auto"
+          >
+            <ListGroup>
+              {members.map((member) => (
+                <ListGroup.Item
+                  className="overflow-auto d-flex align-items-center"
+                  style={{ width: '350px', height: '35px' }}
+                >
+                  <Row className="d-flex">
+                    <Col className="me-3" style={{ width: '275px' }}>
+                      {member}{' '}
+                    </Col>
+                    <Col
+                      className="d-flex justify-content-end"
+                      style={{ witdh: '100px' }}
+                    >
+                      {edit && (
+                        <CloseButton
+                          onClick={() => {
+                            handleShow();
+                            setDelete(member);
+                          }}
+                        ></CloseButton>
+                      )}
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+              ))}
+            </ListGroup>
+          </Col>
+          <Col></Col>
+        </Row>
+        <Row>
+          <Col></Col>
+          <Col className="d-flex justify-content-center align-items-center mx-auto">
+            {edit && <AddGroupMembersForm></AddGroupMembersForm>}
+          </Col>
+          <Col></Col>
+        </Row>
+      </Container>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Remove Email</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Are you sure you want to remove user</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button
+            variant="primary"
+            onClick={() => {
+              handleClose();
+              setMembers(members.filter((m) => m !== del_email));
+            }}
+          >
+            Delete user
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </DefaultLayout>
   );
-};
-
-export default GroupPage;
+}
