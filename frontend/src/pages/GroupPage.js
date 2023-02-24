fcdbf08bf07eeafc8611189a6f976f53522a30f1
wesleykam <wesley_kam@ucsb.edu>
2023-02-24 15:43:01 -0800

    wk - add back delete group button

--- frontend/src/pages/GroupPage.js
+++ frontend/src/pages/GroupPage.js
@@ -9,6 +9,7 @@ import {
   Col,
 } from 'react-bootstrap';
 import AddGroupMembersForm from '../components/forms/AddGroupMembersForm';
+import DeleteGroupButton from '../components/Buttons/DeleteGroupButton';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { config } from '../Constants';
@@ -203,10 +204,16 @@ export default function GroupDetails({ user }) {
               </Col>
             </Row>
             <Row>
-              
               <Col></Col>
               <Col className="d-flex justify-content-center align-items-center mx-auto">
                 {edit && <AddGroupMembersForm></AddGroupMembersForm>}
+              </Col>
+              <Col></Col>
+            </Row>
+            <Row>
+              <Col></Col>
+              <Col style={{paddingTop: '5%'}} className="d-flex justify-content-center align-items-center mx-auto">
+                {edit && <DeleteGroupButton groupId={groupId}></DeleteGroupButton>}
               </Col>
               <Col></Col>
             </Row>
