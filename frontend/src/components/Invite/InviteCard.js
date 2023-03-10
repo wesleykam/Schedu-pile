a2395228bfbd5eaa7b7d3a6d523830d6c178b36d
wesleykam <wesley_kam@ucsb.edu>
2023-03-09 21:12:52 -0800

    Wk -  invite button load (#205)
    * wk - add invites array to user model
    
    * wk - create backend invite send
    
    * wk - create onClick actions for accept and decline buttons
    
    * ll-Need to fix state
    
    * ll - Preserved state through sessionStorage
    
    * ll - fix white screen
    
    * wk - create API for send accept and decline invites
    
    * wk - connect backend to invite card buttons
    
    * wk - fix decline invites
    
    * wk - fix urls
    
    * wk - invite buttons change to spinners while loading
    
    * wk - make load stop
    
    ---------
    
    Co-authored-by: Luke Li <lukewli@ucsb.edu>

--- frontend/src/components/Invite/InviteCard.js
+++ frontend/src/components/Invite/InviteCard.js
@@ -1,24 +1,27 @@
-import React from "react";
-import { useNavigate } from 'react-router-dom';
+import { React, useState } from "react";
+import { ClipLoader } from 'react-spinners';
 
 import { Card, Row, Col, Button } from 'react-bootstrap';
 
 import { acceptInvites, declineInvites } from '../../lib/handleInvites';
 
 
-function inviteCard( { user, groupId, groupName, setGroups, setInvites }) {
+function InviteCard({ user, groupId, groupName, setGroups, setInvites }) {
+    const [loadingAccept, setLoadingAccept] = useState(false);
+    const [loadingDecline, setLoadingDecline] = useState(false);
+
     function acceptInvite() {
-        console.log('accept invite')
         acceptInvites(user, groupId).then((data) => {
             setGroups(data.groups);
             setInvites(data.user.invites);
+            setLoadingAccept(false);
         })
     }
-    
+
     function declineInvite() {
-        console.log('decline invite')
         declineInvites(user, groupId).then((data) => {
             setInvites(data.user.invites);
+            setLoadingDecline(false);
         })
     }
 
@@ -26,12 +29,16 @@ function inviteCard( { user, groupId, groupName, setGroups, setInvites }) {
         <Card className="m-2">
             <Card.Body style={{ width: '100%', cursor: 'pointer' }}>
                 <Row>
-                    <Col style={{display:'flex', alignItems:'center'}}>
-                        <Card.Title>{groupName}</Card.Title> 
+                    <Col style={{ display: 'flex', alignItems: 'center' }}>
+                        <Card.Title>{groupName}</Card.Title>
                     </Col>
-                    <Col style={{display:'flex', justifyContent:'end'}}>
-                        <Button className='m-1' variant='success' onClick={() => {acceptInvite()}}>&#x2713;</Button>
-                        <Button className='m-1' variant='danger' onClick={() => {declineInvite()}}>&#x2715;</Button>
+                    <Col style={{ display: 'flex', justifyContent: 'end' }}>
+                        <Button className='m-1' variant='success' style={{ width: '40px'}} onClick={() => { setLoadingAccept(true); acceptInvite(); }}>
+                            {loadingAccept ? <ClipLoader color="#FFFFFF" size={13} /> : <span>&#x2713;</span>}
+                        </Button>
+                        <Button className='m-1' variant='danger' style={{ width: '40px'}} onClick={() => { setLoadingDecline(true); declineInvite(); }}>
+                            {loadingDecline ? <ClipLoader color="#FFFFFF" size={13} /> : <span>&#x2715;</span>}
+                        </Button>
                     </Col>
                 </Row>
             </Card.Body>
@@ -39,4 +46,4 @@ function inviteCard( { user, groupId, groupName, setGroups, setInvites }) {
     )
 }
 
-export default inviteCard
+export default InviteCard
